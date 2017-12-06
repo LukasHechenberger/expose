1e6a4a2b6c5c7b12019e0bbf91b67df522af30c0
Lukas Hechenberger <admin@ls-age.com>
2017-12-06 18:48:57 +0100

    style: Add main export

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Expose from './lib/Expose';
+
+export default Expose;
+export { default as Command } from './lib/Command';
+export { default as Option } from './lib/Option';
+export { default as Boolean } from './lib/Options/Boolean';
+export { default as Number } from './lib/Options/Number';
+export { default as String } from './lib/Options/String';
